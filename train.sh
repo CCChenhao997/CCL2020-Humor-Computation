e8369fc2979de67cab90275ea7751e03fb0a9369
CCChenhao997 <ccchenhao997@gmail.com>
2020-06-23 23:41:08 +0800

    train ernie for cn

--- train.sh
+++ train.sh
@@ -3,4 +3,4 @@
 # python train.py --model bert_spc --dataset en --seed 1000 --learning_rate 2e-5 --num_epoch 3 --max_length 80 --pretrained_bert_name bert-large-uncased --bert_dim 1024
 # python train.py --model bert_spc --dataset en --seed 1000 --learning_rate 2e-5 --num_epoch 3 --max_length 80
 
-python train.py --model bert_spc --dataset cn --seed 1000 --learning_rate 2e-5 --num_epoch 3 --max_length 80 --pretrained_bert_name bert-base-chinese
+python train.py --model bert_spc --dataset cn --seed 1000 --learning_rate 2e-5 --num_epoch 3 --max_length 80 --batch_size 32
