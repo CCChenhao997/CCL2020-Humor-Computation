577621a9762ba1851d18c45a229d8d1f56ca6d9a
CCChenhao997 <ccchenhao997@gmail.com>
2020-06-27 08:25:45 +0800

    delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# bert_att
-# python train_k_fold_cross_val.py --model bert_att --dataset en --seed 1000 --learning_rate 2e-5 --num_epoch 3  # bert_base
-python train_k_fold_cross_val.py --model bert_att --dataset cn --pretrained_bert_name ./pretrain_models/ERNIE_cn  --seed 1000 --learning_rate 2e-5 --num_epoch 3  # ERNIE
