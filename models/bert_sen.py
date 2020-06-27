94bf4970b7db7362bc6c370852fd5fe16b6ca650
CCChenhao997 <ccchenhao997@gmail.com>
2020-06-27 08:30:21 +0800

    change bert.py to bert_sen.py

---
+++ models/bert_sen.py
@@ -0,0 +1,29 @@
+'''
+@Description: 
+@version: 
+@Author: chenhao
+@Date: 2020-06-23 23:36:02
+@LastEditors: chenhao
+@LastEditTime: 2020-06-25 22:50:16
+'''
+
+import torch
+import torch.nn as nn
+
+
+class BERT(nn.Module):
+    def __init__(self, bert, opt):
+        super(BERT, self).__init__()
+        self.bert = bert
+        self.dropout = nn.Dropout(opt.dropout)
+        # self.dense = nn.Linear(opt.bert_dim, opt.polarities_dim)
+        layers = [nn.Linear(
+            opt.bert_dim, 256), nn.ReLU(), nn.Linear(256, opt.polarities_dim)]
+        self.dense = nn.Sequential(*layers)
+
+    def forward(self, inputs):
+        text_bert_indices, attention_mask = inputs[0], inputs[1]
+        _, pooled_output = self.bert(text_bert_indices, attention_mask=attention_mask)
+        pooled_output = self.dropout(pooled_output)
+        logits = self.dense(pooled_output)
+        return logits
