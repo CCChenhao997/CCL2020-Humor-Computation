a0a292d5add41bccd9f0f577db8b65623e1df37b
airflier <335165281@qq.com>
2020-07-12 10:49:57 +0800

    Improve the predict.py

--- predict.py
+++ predict.py
@@ -16,6 +16,7 @@ from models.bert_att import BERT_Att
 from models.bert_spc_att import BERT_SPC_Att
 from models.bert_spc_pos import BERT_SPC_Pos
 from models.bert_spc_cap import BERT_SPC_Cap
+from models.bert_spc_lay import BERT_SPC_Lay
 from data_utils import Tokenizer4Bert, BertSentenceDataset
 
 
@@ -29,6 +30,7 @@ class Inferer:
         self.model = opt.model_class(bert_model, opt).to(opt.device)
 
         print('loading model {0} ...'.format(opt.model_name))
+        
         self.model.load_state_dict(torch.load(opt.state_dict_path))
         self.model = self.model.to(opt.device)
         
@@ -52,7 +54,11 @@ def get_parameters():
     # Hyperparameters
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', default='bert_spc', type=str, help=', '.join(model_classes.keys()))
-    # parser.add_argument('--dataset', default='en', type=str, help=', '.join(dataset_files.keys()))
+    parser.add_argument('--dataset', default='en', type=str, help=', '.join(dataset_files.keys()))
+    parser.add_argument('--date', default='date', type=str)
+    parser.add_argument('--fold_n', default=0, type=str)
+    parser.add_argument('--pseudo', default='False', type=str)
+    
     parser.add_argument('--learning_rate', default=0.002, type=float)    # 1e-3
     parser.add_argument('--dropout', default=0.5, type=float)
     parser.add_argument('--l2reg', default=1e-5, type=float)    # 1e-5
@@ -60,8 +66,8 @@ def get_parameters():
     parser.add_argument('--batch_size', default=16, type=int)
     parser.add_argument('--log_step', default=5, type=int)
     # parser.add_argument('--embed_dim', default=300, type=int)
-    # parser.add_argument('--hidden_dim', default=200, type=int)
-    # parser.add_argument('--position_dim', default=100, type=int)
+    parser.add_argument('--hidden_dim', default=300, type=int)
+    parser.add_argument('--position_dim', default=100, type=int)
     parser.add_argument('--polarities_dim', default=2, type=int, help='2')
     parser.add_argument('--max_length', default=80, type=int)
     parser.add_argument('--device', default=None, type=str, help='cpu, cuda')
@@ -91,21 +97,26 @@ if __name__=="__main__":
         'bert_spc_att': BERT_SPC_Att,
         'bert_spc_pos': BERT_SPC_Pos,
         'bert_spc_cap': BERT_SPC_Cap,
+        'bert_spc_lay': BERT_SPC_Lay,
     }
 
     model_state_dict_paths = {
-        # 'bert_spc': './recorder/bert_spc_0703_adv/en/bert_spc_en_fold_0_score_1.4303',
-        # 'bert_spc': './recorder/bert_spc_0703_adv/en/bert_spc_en_fold_1_score_1.4434',
-        # 'bert_spc': './recorder/bert_spc_0703_adv/en/bert_spc_en_fold_2_score_1.4481',
-        # 'bert_spc': './recorder/bert_spc_0703_adv/en/bert_spc_en_fold_3_score_1.4569',
-        # 'bert_spc': './recorder/bert_spc_0703_adv/en/bert_spc_en_fold_4_score_1.4566',
+        
+        'en':{
+            '0': './recorder/bert_spc_lay_0711/en_uuu_adv/bert_spc_lay_en_fold_0_f1_0.6630_f1_0_0.8253_f1_1_0.5006',
+            '1': './recorder/bert_spc_lay_0711/en_raw/bert_spc_lay_en_fold_1_f1_0.6705_f1_0_0.8287_f1_1_0.5122',
+            '2': './recorder/bert_spc_lay_0711/en_raw_adv/bert_spc_lay_en_fold_2_f1_0.6870_f1_0_0.8473_f1_1_0.5267',
+            '3': './recorder/bert_spc_lay_0711/en_uuu_adv/bert_spc_lay_en_fold_3_f1_0.6835_f1_0_0.8494_f1_1_0.5176',
+            '4': './recorder/bert_spc_lay_0711/en_raw_adv/bert_spc_lay_en_fold_4_f1_0.6958_f1_0_0.8550_f1_1_0.5365',
+        },
 
-        # 'bert_spc': './recorder/bert_spc_0703_adv/cn/bert_spc_cn_fold_0_score_1.3849',
-        # 'bert_spc': './recorder/bert_spc_0703_adv/cn/bert_spc_cn_fold_1_score_1.3520',
-        # 'bert_spc': './recorder/bert_spc_0703_adv/cn/bert_spc_cn_fold_2_score_1.3619',
-        # 'bert_spc': './recorder/bert_spc_0703_adv/cn/bert_spc_cn_fold_3_score_1.3773',
-        # 'bert_spc': './recorder/bert_spc_0703_adv/cn/bert_spc_cn_fold_4_score_1.3710',
-
+        'cn':{
+            '0': './recorder/pick/cn/lay_adv_711/bert_spc_lay_cn_fold_0_f1_0.6606_f1_0_0.7744_f1_1_0.5469',
+            '1': './recorder/pick/cn/lay_adv_711/bert_spc_lay_cn_fold_1_f1_0.6394_f1_0_0.7496_f1_1_0.5292',
+            '2': './recorder/pick/cn/lay_adv_711/bert_spc_lay_cn_fold_2_f1_0.6421_f1_0_0.7605_f1_1_0.5236',
+            '3': './recorder/pick/cn/lay_adv_711/bert_spc_lay_cn_fold_3_f1_0.6237_f1_0_0.7242_f1_1_0.5231',
+            '4': './recorder/pick/cn/lay_adv_711/bert_spc_lay_cn_fold_4_f1_0.6528_f1_0_0.7800_f1_1_0.5255',
+        }
         # * 伪标签模型
         # 'bert_spc': './recorder/bert_spc_0703_adv_pseudo/en/bert_spc_en_fold_0_score_1.4734',
         # 'bert_spc': './recorder/bert_spc_0703_adv_pseudo/en/bert_spc_en_fold_1_score_1.4553',
@@ -129,6 +140,11 @@ if __name__=="__main__":
             'test': './data/preprocess/en_test.tsv'
         }
     }
+
+    pretrained_bert_names = {
+        'cn':  './pretrain_models/ERNIE_cn',
+        'en':  'bert-base-uncased'
+    }
     
     input_colses = {
         'bert': ['sentence_bert_indices', 'attention_mask'],
@@ -137,26 +153,28 @@ if __name__=="__main__":
         'bert_spc_att': ['sentence_pair_bert_indices', 'bert_segments_ids', 'attention_mask_pair'],
         'bert_spc_pos': ['sentence_pair_bert_indices', 'bert_segments_ids', 'attention_mask_pair'],
         'bert_spc_cap': ['sentence_pair_bert_indices', 'bert_segments_ids', 'attention_mask_pair'],
+        'bert_spc_lay': ['sentence_pair_bert_indices', 'bert_segments_ids', 'attention_mask_pair'],
     }
 
     opt = get_parameters()
     #! 注意
-    opt.dataset = 'cn'
-    date = "0705"
-    fold_n = 4
-    pseudo = True
+    # opt.dataset = 'cn'
+    # date = "0711"
+    # fold_n = 0
+    # pseudo = True
     #! 模型路径
     # opt.pretrained_bert_name = 'bert-base-uncased'
-    opt.pretrained_bert_name = './pretrain_models/ERNIE_cn'
+    # opt.pretrained_bert_name = './pretrain_models/ERNIE_cn'
 
     #! max_length
-    if opt.dataset == 'cn':
-        opt.max_length = 128
+    # if opt.dataset == 'cn':
+    #     opt.max_length = 128
 
     opt.dataset_file = dataset_files[opt.dataset]
+    opt.pretrained_bert_name = pretrained_bert_names[opt.dataset]
     opt.inputs_cols = input_colses[opt.model_name]
     opt.model_class = model_classes[opt.model_name]
-    opt.state_dict_path = model_state_dict_paths[opt.model_name]
+    opt.state_dict_path = model_state_dict_paths[opt.dataset][opt.fold_n]
     opt.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     inf = Inferer(opt)
@@ -165,12 +183,12 @@ if __name__=="__main__":
 
     predict_df = pd.DataFrame(list(zip(id, predict_label)))
 
-    if pseudo:
-        save_path = "./predict_data/{}_{}_pseudo/{}".format(opt.model_name, date, opt.dataset)
+    if opt.pseudo != "False":
+        save_path = "./predict_data/{}_{}_pseudo/{}".format(opt.model_name, opt.date, opt.dataset)
     else:
-        save_path = "./predict_data/{}_{}/{}".format(opt.model_name, date, opt.dataset)
+        save_path = "./predict_data/{}_{}/{}".format(opt.model_name, opt.date, opt.dataset)
     if not os.path.exists(save_path):
         os.makedirs(save_path, mode=0o777)
     
-    file_path = "{}/{}-{}-fold-{}.csv".format(save_path, opt.model_name, opt.dataset, fold_n)
+    file_path = "{}/{}-{}-fold-{}.csv".format(save_path, opt.model_name, opt.dataset, opt.fold_n)
     predict_df.to_csv(file_path, index=None, header=['ID', 'Label'])
---
+++ predict.sh
@@ -0,0 +1,12 @@
+# python predict.py --model_name bert_spc_lay --dataset cn --date 0711 --fold_n 0 --pseudo False --max_length 128
+# python predict.py --model_name bert_spc_lay --dataset cn --date 0711 --fold_n 1 --pseudo False --max_length 128
+# python predict.py --model_name bert_spc_lay --dataset cn --date 0711 --fold_n 2 --pseudo False --max_length 80
+# python predict.py --model_name bert_spc_lay --dataset cn --date 0711 --fold_n 3 --pseudo False --max_length 128
+# python predict.py --model_name bert_spc_lay --dataset cn --date 0711 --fold_n 4 --pseudo False --max_length 128
+
+
+python predict.py --model_name bert_spc_lay --dataset en --date 0711 --fold_n 0 --pseudo False
+python predict.py --model_name bert_spc_lay --dataset en --date 0711 --fold_n 1 --pseudo False
+python predict.py --model_name bert_spc_lay --dataset en --date 0711 --fold_n 2 --pseudo False
+python predict.py --model_name bert_spc_lay --dataset en --date 0711 --fold_n 3 --pseudo False
+python predict.py --model_name bert_spc_lay --dataset en --date 0711 --fold_n 4 --pseudo False
